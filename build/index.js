785e0b5809f5a273157d77d0007295eee6594b32
Ashish Mohite <mohite.aashish.1@gmail.com>
2020-11-07 18:55:12 +0530

    Add node modules

--- build/index.js
+++ build/index.js
@@ -1,35 +1,67 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var core_1 = __importDefault(require("@actions/core"));
+var core = __importStar(require("@actions/core"));
 var requests_1 = require("./requests");
 try {
-    var webhookUrl = core_1.default.getInput("webhook_url")
-        ? core_1.default.getInput("webhook_url")
+    var webhookUrl = core.getInput("webhook_url")
+        ? core.getInput("webhook_url")
         : process.env.WEBHOOK_URL
             ? process.env.WEBHOOK_URL
             : "";
-    var headers = core_1.default.getInput("headers")
-        ? core_1.default.getInput("headers")
-        : process.env.HEADERS
-            ? process.env.HEADERS
-            : "";
-    var method = core_1.default.getInput("method")
-        ? core_1.default.getInput("method")
+    var method = core.getInput("method")
+        ? core.getInput("method")
         : process.env.METHOD
             ? process.env.METHOD
             : "GET";
-    var body = core_1.default.getInput("body")
-        ? core_1.default.getInput("body")
+    var headers = core.getInput("headers")
+        ? core.getInput("headers")
+        : process.env.HEADERS
+            ? process.env.HEADERS
+            : "{}";
+    try {
+        JSON.parse(headers);
+    }
+    catch (e) {
+        core.setFailed(e.message);
+    }
+    var body = core.getInput("body")
+        ? core.getInput("body")
         : process.env.BODY
             ? process.env.BODY
-            : "";
-    requests_1.sendRequest(webhookUrl, method, headers, body).then(function (data) {
-        console.log(data);
+            : "{}";
+    try {
+        JSON.parse(body);
+    }
+    catch (e) {
+        core.setFailed(e.message);
+    }
+    requests_1.sendRequest(webhookUrl, method, headers, body)
+        .then(function (data) {
+        core.setOutput("response", data);
+    })
+        .catch(function (err) {
+        core.setFailed(err.message);
     });
 }
 catch (error) {
-    core_1.default.setFailed(error.message);
+    core.setFailed(error.message);
 }
--- build/requests.js
+++ build/requests.js
@@ -1,4 +1,23 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -35,28 +54,30 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sendRequest = void 0;
-var node_fetch_1 = __importDefault(require("node-fetch"));
+var node_fetch_1 = __importStar(require("node-fetch"));
 function requests(url, method, headers, data) {
-    if (url === void 0) { url = ''; }
-    if (data === void 0) { data = {}; }
+    if (url === void 0) { url = ""; }
+    if (method === void 0) { method = "GET"; }
+    if (headers === void 0) { headers = ""; }
     return __awaiter(this, void 0, void 0, function () {
-        var response;
+        var params, response;
         return __generator(this, function (_a) {
             switch (_a.label) {
-                case 0: return [4 /*yield*/, node_fetch_1.default(url, {
+                case 0:
+                    params = {
                         method: method,
-                        headers: JSON.parse(headers),
-                        redirect: 'follow',
-                        body: JSON.stringify(data)
-                    })];
+                        headers: new node_fetch_1.Headers(JSON.parse(headers)),
+                    };
+                    if (method != "GET") {
+                        params.body = data;
+                    }
+                    return [4 /*yield*/, node_fetch_1.default(url, params)];
                 case 1:
                     response = _a.sent();
-                    return [2 /*return*/, response.json()];
+                    return [4 /*yield*/, response.text()];
+                case 2: return [2 /*return*/, _a.sent()];
             }
         });
     });
